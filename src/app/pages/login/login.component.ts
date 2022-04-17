166023dd673e9e207b7a1992f89d2cdc348018fd
scania471 <martin.terhes@icloud.com>
2022-04-17 20:41:23 +0200

    Auth service v0.1

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,23 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormControl } from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {Subscriber, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   email = new FormControl('')
   passwd = new FormControl('')
 
-  constructor() { }
+  loadingSubscription?: Subscription;
+
+  constructor(private loadingService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.email.value, this.passwd.value)
+      console.log(this.email.value, this.passwd.value)
+
+      // this.loadingService.loading_test1(this.email.value, this.passwd.value)
+      //     .then((data: boolean) => {
+      //           console.log("success")
+      //           this.router.navigateByUrl("/main")
+      //       })
+      //     .catch((error) => {
+      //           console.error(error)
+      //       })
+      //     .finally(() => {
+      //           console.log("finallyyyy")
+      //       })
+
+      this.loadingSubscription = this.loadingService.loading_test2(this.email.value, this.passwd.value)
+          .subscribe((data: boolean) => {
+            console.log(data)
+          })
   }
+
+  ngOnDestroy() {
+      console.log("destroyed")
+      this.loadingSubscription?.unsubscribe()
+  }
+
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import {Observable, Subscriber} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  loading_test1(email: string, passwd: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (email === "e@e.e" && passwd === "1111") {
+          resolve(true)
+        } else {
+          reject(false)
+        }
+      }, 2000)
+    })
+  }
+
+  loading_test2(email: string, passwd: string): Observable<boolean> {
+    return new Observable((subscriber: Subscriber<boolean>) => {
+      let i = 0
+      const interval = setInterval(() => {
+        i++
+        if (i === 2) {
+          if (email === "e@e.e" && passwd === "1111") {
+            subscriber.next(true)
+            subscriber.complete()
+          } else {
+            subscriber.error(false)
+          }
+        }
+      }, 1000)
+    })
+  }
 
   constructor() { }
 }
